859baa4706587c79065861681f6a56a2a355de8c
tommy <nnhtommy@uw.edu>
2025-11-26 16:10:23 -0800

    removing unncessary register in cpmg

--- src/qickdawg/arqick/arqick_cpmg.py
+++ src/qickdawg/arqick/arqick_cpmg.py
@@ -49,8 +49,6 @@ class CPMG(NVAveragerProgram):
                                                name='delay', 
                                                init_val=self.cfg.delay_start_treg)
 
-        self.mw_time_register = self.get_gen_reg(self.cfg.mw_channel, 
-                                                 name='t') # idk why this is needed. should check this later
 
         self.n_cpmg_register = self.new_gen_reg(
             self.cfg.mw_channel,
