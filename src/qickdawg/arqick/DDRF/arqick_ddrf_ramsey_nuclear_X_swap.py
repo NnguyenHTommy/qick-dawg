9ea1dedc49df239a969886177fad4a61a04ae3e6
Tommy <nnguyen.h.tommy@gmail.com>
2026-05-20 16:54:25 -0700

    ddrf X tomography ramsey and end phase with N cycle

---
+++ src/qickdawg/arqick/DDRF/arqick_ddrf_ramsey_nuclear_X_swap.py
@@ -0,0 +1,103 @@
+'''
+Ramsey nuclear X with reduced swap in beginning with DDRF
+'''
+
+from qickdawg.nvpulsing.nvaverageprogram import NVAveragerProgram
+from qickdawg.nvpulsing.nvqicksweep import NVQickSweep
+from qickdawg.arqick.standard_ops import StandardOps
+
+class RamseyNuclearXSwapDDRF(StandardOps, NVAveragerProgram):
+
+    required_cfg = [
+        # params that usually won't change
+        "mw_channel",  # MW channel
+        "mw_nqz",  # 1 at 1405 MHz
+        "reps",
+        "pmod_out_pin",  # should be 0 for PMOD0_0
+        "pmod_out_pulse_width_treg",  # 50ns is reasonable
+        "pmod_out_trig_delay_treg",  # delay between trigger and pulse seq start
+        "inherent_trigger_to_pulses_delay_treg",  # should be 209.27ns
+        "pulse_seq_delay_treg",  # delay between sequence end and next trigger start
+        
+        # for the normal pulse part
+        "mw_gain",  # MW gain
+        "freq_freg",  # microwave freq
+        "mw_pi2_tdds",  # length of pi/2 pulse
+
+        "delay_tdds_start",
+        "delay_tdds_end",
+        "nsweep_points",
+        "C13_measurement_delay_tdds",
+        "delay_tdds_gate_crxpi2",
+        "n_cpmg_gate_crxpi2",
+    ]
+
+    def initialize(self):
+        self.init()
+
+        self.delay_register = self.new_gen_reg(
+            self.cfg.mw_channel,
+            name='delay',
+            init_val=0,
+        )
+
+        self.add_sweep(
+            NVQickSweep(
+                self,
+                self.delay_register,
+                self.cfg.delay_tdds_start,
+                self.cfg.delay_tdds_end,
+                self.cfg.nsweep_points,
+            )
+        )
+        
+
+        self.synci(200)  # give processor some time to configure pulses
+
+    def body(self):
+        self.pmod_trigger_sequence()
+        self.tdds_offset_register.reset()
+
+        # NUCLEAR SPIN INITIALIZATION 
+        # e RY(pi/2) 
+        self.set_pulse_registers(ch=self.cfg.mw_channel, waveform="half_pi_0", freq=self.cfg.freq_freg, gain=self.cfg.mw_gain, phase=self.deg2reg(90))
+        self.pulse(ch=self.cfg.mw_channel)
+        self.sync_all()
+
+        # e-n CROTX(pi/2)
+        self.cpmg_xy8_gate(gate_index=0, pi_2_pulse_before=True, delay_tau_tdds=self.cfg.delay_tdds_gate_crxpi2, n_cpmg_pulses=self.cfg.n_cpmg_gate_crxpi2)
+
+        # e RX(pi/2)
+        self.set_pulse_registers(ch=self.cfg.mw_channel, waveform="half_pi_0", freq=self.cfg.freq_freg, gain=self.cfg.mw_gain, phase=self.deg2reg(0))
+        self.offset_computations(pi2_after=True, delay_tau_tdds=self.cfg.delay_tdds_gate_crxpi2)
+        self.sync(self.treg_offset_register.page, self.treg_offset_register.addr)
+        self.pulse(ch=self.cfg.mw_channel)
+        self.sync_all()
+
+        # e-n CROTY(-+pi/2)
+        self.cpmg_xy8_gate(gate_index=1, pi_2_pulse_before=True, delay_tau_tdds=self.cfg.delay_tdds_gate_crxpi2, n_cpmg_pulses=self.cfg.n_cpmg_gate_crxpi2)
+
+        # wait for optical pump
+        self.tdds_offset_register.set_to(self.tdds_offset_register, '+', self.cfg.C13_measurement_delay_tdds)
+        
+        # e-n CROTX(pi/2)
+        self.cpmg_xy8_gate(gate_index=2, pi_2_pulse_before=False, delay_tau_tdds=self.cfg.delay_tdds_gate_crxpi2, n_cpmg_pulses=self.cfg.n_cpmg_gate_crxpi2)
+        # ramsey wait tau
+        self.tdds_offset_register.set_to(self.tdds_offset_register, '+', self.delay_register)
+
+       # e RY(pi/2)
+        self.set_pulse_registers(ch=self.cfg.mw_channel, waveform="half_pi_0", freq=self.cfg.freq_freg, gain=self.cfg.mw_gain, phase=self.deg2reg(90))
+        self.offset_computations(pi2_after=True, delay_tau_tdds=self.cfg.delay_tdds_gate_crxpi2)
+        self.sync(self.treg_offset_register.page, self.treg_offset_register.addr)
+        self.pulse(ch=self.cfg.mw_channel)
+        self.sync_all()
+
+        # e-n CROTX(pi/2)
+        self.cpmg_xy8_gate(gate_index=3, pi_2_pulse_before=True, delay_tau_tdds=self.cfg.delay_tdds_gate_crxpi2, n_cpmg_pulses=self.cfg.n_cpmg_gate_crxpi2)
+
+        # e RX(pi/2)
+        self.set_pulse_registers(ch=self.cfg.mw_channel, waveform="half_pi_0", freq=self.cfg.freq_freg, gain=self.cfg.mw_gain, phase=self.deg2reg(0))
+        self.offset_computations(pi2_after=True, delay_tau_tdds=self.cfg.delay_tdds_gate_crxpi2)
+        self.sync(self.treg_offset_register.page, self.treg_offset_register.addr)
+        self.pulse(ch=self.cfg.mw_channel)
+        self.sync_all(self.cfg.pulse_seq_delay_treg)
---
+++ src/qickdawg/arqick/DDRF/arqick_ddrf_spec_noendphase_sweep.py
@@ -0,0 +1,57 @@
+'''
+DDRf spec with no end phase so just set the last pi/2 pulse to be the correct phase
+=======================================================================
+Min resolution of 200ps for delay steps between pulses in CPMG XY8 sequence
+using fine control of waveform start address and phase.
+'''
+
+from qickdawg.nvpulsing.nvaverageprogram import NVAveragerProgram
+from qickdawg.arqick.standard_ops import StandardOps
+
+class DDRFSpecFineRes(StandardOps, NVAveragerProgram):
+    '''
+    DDRF CPMG XY8 sub-nanosecond resolution pulsing program with DDRF
+    '''
+    required_cfg = [        
+        "mw_pi2_tdds", # length of pi/2 pulse
+        "freq_freg", # Microwave freq 
+        "mw_channel", # MW Channel
+        "mw_nqz", # 1 at 1405 MHz
+        "mw_gain", # MW Gain
+        "reps",
+        "pmod_out_pin", # should be 0 for PMOD0_0
+        "pmod_out_pulse_width_treg", # 50ns is reasonable
+        "pmod_out_trig_delay_treg", # delay between trigger and pulse seq start. this is added to the already 198 inherent ns delay so putting 300 means 198+300=498ns delay
+        "inherent_trigger_to_pulses_delay_treg", # should be 209.27ns 
+        "pulse_seq_delay_treg", # delay between pulse seq end and trigger start of next seq
+        
+        "delay_tdds",
+        "n_cpmg", 
+    ]
+
+    def initialize(self):
+        self.init()     
+        self.synci(200)  # give processor some time to configure pulses
+
+    def body(self):
+        self.pmod_trigger_sequence()
+
+        self.tdds_offset_register.reset()
+        self.set_pulse_registers(ch=self.cfg.mw_channel, waveform="half_pi_0", freq=self.cfg.freq_freg, gain=self.cfg.mw_gain, phase=self.deg2reg(90))
+        self.pulse(ch=self.cfg.mw_channel)
+        self.sync_all()
+
+        self.cpmg_xy8_gate(gate_index=0, pi_2_pulse_before=True, delay_tau_tdds=self.cfg.delay_tdds, n_cpmg_pulses=self.cfg.n_cpmg)
+
+        # Final tau and readout pi/2 pulse.
+        self.set_pulse_registers(
+            ch=self.cfg.mw_channel,
+            waveform="half_pi_0",
+            freq=self.cfg.freq_freg,
+            gain=self.cfg.mw_gain,
+            phase=self.deg2reg(0),
+        )
+        self.offset_computations(pi2_after=True, delay_tau_tdds=self.cfg.delay_tdds)
+        self.sync(self.treg_offset_register.page, self.treg_offset_register.addr)
+        self.pulse(ch=self.cfg.mw_channel)
+        self.sync_all(self.cfg.pulse_seq_delay_treg)
